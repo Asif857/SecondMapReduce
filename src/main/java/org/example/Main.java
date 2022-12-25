f7032eeff8d466e669ab8215b5ca9f94fdafc9ee
assiph <asif857@gmail.com>
2022-12-25 14:57:29 +0200

    Finito, need to think about combiner

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -23,68 +23,43 @@ public class Main {
             while (itr.hasMoreTokens()) {
                 String currKey = itr.nextToken();
                 String val = itr.nextToken();
-                word.set(currKey);
+                word.set("0" + "," + currKey);
+                Text wordPlusOne = new Text("1" + "," + currKey);
                 String[] values = val.split(",");
-                if ("m_X,pleased,_ADP_".equals(currKey)){
-                    System.out.println(new Text("0N" + values[0]) + " value: " + word);
-                    System.out.println(new Text("1N" + values[1])+ " value: " + word);
-                    System.out.println(new Text("0T" + values[0])+ " value: " + values[1] + "," + currKey);
-                    System.out.println(new Text("1N" + values[1])+ " value: " + values[0] + "," + currKey);
-                }
-                context.write(new Text("0N" + values[0]),word);
-                context.write(new Text("1N" + values[1]),word);
+                context.write(new Text("0N" + values[0]),wordPlusOne);
+                context.write(new Text("1N" + values[1]),wordPlusOne);
                 context.write(new Text("0T" + values[0]),new Text(values[1] + "," + currKey));
                 context.write(new Text("1T" + values[1]),new Text(values[0] + "," + currKey));
+                context.write(new Text("r" + values[2]),new Text(word));
             }
         }
     }
-    //0NV   m_X,pleased,_ADP_
     public static class ParametersReducer
             extends Reducer<Text,Text,Text,Text> {
+        float N0 = 0;
+        float N1 = 0;
+        float T0 = 0;
+        float T1 = 0;
         public void reduce(Text key, Iterable<Text> values,
                            Context context
         ) throws IOException, InterruptedException {
-            if (key.toString().charAt(1) == 'N'){
-                int N = 0;
-                int i = 0;
-                Text textOfN = new Text();
-                if (key.toString().equals("0N10")) {
-                    System.out.println("changed");
-                    i = 1;
+            if (key.toString().charAt(0) == 'r'){
+                float N = Float.parseFloat(context.getConfiguration().get("N"));
+                for (Text value: values){
+                    float pr = (T1 + T0)/(N*(N1 + N0));
+                    String newkey = key.toString().replaceAll(",", " ");
+                    context.write(new Text(newkey),new Text(String.valueOf(pr)));
                 }
-                ArrayList<String> valuesTextArray = new ArrayList<>();
-                for (Text value : values){
-                    if (i==1){
-                        System.out.println("value: " + value.toString() + "key: " + key.toString());
-                    }
-                    valuesTextArray.add(value.toString());
-                    N += 1;
-                }
-                textOfN.set(key.toString().substring(0,2) + N);
-                for (String value : valuesTextArray){
-                    if (i==1){
-                        System.out.println("value To Enter: " + value.toString() + "key: " + key.toString());
-                    }
-                    context.write(new Text(value),textOfN);
-                }
+                 N0 = 0;
+                 N1 = 0;
+                 T0 = 0;
+                 T1 = 0;
             }
-            else if (key.toString().charAt(1) == 'T'){
-                int valueOfT = 0;
-                ArrayList<Text> valuesTextArray = new ArrayList<>();
-                for (Text value : values){
-                    String[] splitValueByComma = value.toString().split(",");
-                    int recordValue = Integer.parseInt(splitValueByComma[0]);
-                    valueOfT += recordValue;
-                    Text keyWithoutStartingNumber = new Text();
-                    keyWithoutStartingNumber.set(splitValueByComma[1] + "," + splitValueByComma[2] + "," + splitValueByComma[3]);
-                    valuesTextArray.add(keyWithoutStartingNumber);
+            else {
+                switch (key.toString().substring(0,2)){
+                    case "0N":
                 }
-                Text sendValueOfT = new Text();
 
-                for (Text value: valuesTextArray){
-                    sendValueOfT.set(key.toString().substring(0,2) + String.valueOf(valueOfT));
-                    context.write(value,sendValueOfT);
-                }
             }
             }
         }
@@ -98,10 +73,10 @@ public class Main {
         job.setMapOutputValueClass(Text.class);
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(Text.class);
-        //FileInputFormat.addInputPath(job, new Path(args[1]));
-        //FileOutputFormat.setOutputPath(job, new Path(args[2]));
-        FileInputFormat.addInputPath(job, new Path(args[0]));
-        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        FileInputFormat.addInputPath(job, new Path(args[1]));
+        FileOutputFormat.setOutputPath(job, new Path(args[2]));
+        //FileInputFormat.addInputPath(job, new Path(args[0]));
+        //FileOutputFormat.setOutputPath(job, new Path(args[1]));
         System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
 }
